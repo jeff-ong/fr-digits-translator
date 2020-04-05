df0b290f121a2f7b8f75cea45d5419d8160321d1
Jeff Ong <jefftimbre@outlook.com>
2020-04-05 11:36:47 -0400

    test deploy
    Signed-off-by: Jeff Ong <jefftimbre@outlook.com>

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const path = require('path')
 app.use('/',express.static(path.join(__dirname, '../public')));
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('listening on port 3000');
+    console.log('listening on PORTL 3000');
 });
